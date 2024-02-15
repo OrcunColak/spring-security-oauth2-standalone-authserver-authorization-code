3c41db624df39cb8a5e1bd8895b1d85957f536e5
Orçun Çolak <orcun.colak@hazelcast.com>
2024-02-15 14:57:16 +0300

    Initial commit

---
+++ src/main/java/com/colak/springsecurityoauth2standaloneauthserverauthorizationcode/AuthorizationCodeApplication.java
@@ -0,0 +1,13 @@
+package com.colak.springsecurityoauth2standaloneauthserverauthorizationcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationCodeApplication.class, args);
+	}
+
+}
